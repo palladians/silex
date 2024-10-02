a31ce0c7c6f7708d539bf091f6bb405daa60d289
Tomek Marciniak <tmarciniakm@gmail.com>
2024-10-03 00:05:49 +0200

    fix(ci): add base

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,7 @@ const config = {
       fallback: "404.html",
     }),
     paths: {
-      base: process.argv.includes("dev")
-        ? ""
-        : (process.env.BASE_PATH ?? "/silex"),
+      base: process.argv.includes("dev") ? "" : "/silex",
     },
     files: {
       serviceWorker: "src/service-worker.ts",
