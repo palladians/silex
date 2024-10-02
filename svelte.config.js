0d7ec59f1fc5727231db5b5a14f4d521e63b10bc
Tomek Marciniak <tmarciniakm@gmail.com>
2024-10-03 00:02:37 +0200

    fix(ci): add base

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,26 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: "404.html",
-		}),
-		paths: {
-			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH,
-		},
-		files: {
-			serviceWorker: "src/service-worker.ts",
-		},
-	},
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      fallback: "404.html",
+    }),
+    paths: {
+      base: process.argv.includes("dev")
+        ? ""
+        : (process.env.BASE_PATH ?? "/silex"),
+    },
+    files: {
+      serviceWorker: "src/service-worker.ts",
+    },
+  },
 };
 
 export default config;
